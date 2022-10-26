df7a1fe0d96ee300e5d445b92cd077e02c5c179c
dddowon <dowon4095@naver.com>
2022-10-26 22:48:21 +0900

    자료구조 Stack 구현 및 테스트

--- 자료구조 1/Stack/Stack/main.swift
+++ 자료구조 1/Stack/Stack/main.swift
@@ -7,4 +7,37 @@
 
 import Foundation
 
+struct Stack<T> {
+    var stack: [T] = []
 
+    var count: Int {
+        return stack.count
+    }
+    
+    var isEmpty: Bool {
+        return stack.isEmpty
+    }
+    
+    mutating func push(_ element: T) {
+        stack.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        stack.popLast()
+    }
+}
+
+var stack = Stack<Int>()
+
+stack.push(1)
+stack.push(2)
+
+print(stack.stack)
+print(stack.count)
+print(stack.isEmpty)
+
+stack.pop()
+stack.pop()
+
+print(stack.stack)
+print(stack.isEmpty)
