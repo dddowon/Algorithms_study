e3bcbe7f6f881551fb4b01580fc49caaf38ca237
dddowon <dowon4095@naver.com>
2022-10-27 19:30:03 +0900

    자료구조 Queue 구현 및 테스트

--- 자료구조 1/Queue/Queue/main.swift
+++ 자료구조 1/Queue/Queue/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Queue<T> {
+    var queue: [T] = []
+    
+    var count: Int {
+        return queue.count
+    }
+    
+    var isEmpty: Bool {
+        return queue.isEmpty
+    }
+    
+    mutating func enqueue(_ element: T) {
+        queue.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        queue.removeFirst()
+    }
+}
 
+
+var queue = Queue<Int>()
+
+queue.enqueue(1)
+queue.enqueue(2)
+
+print(queue.queue)
+print(queue.count)
+print(queue.isEmpty)
+
+queue.dequeue()
+queue.dequeue()
+
+print(queue.queue)
+print(queue.count)
+print(queue.isEmpty)
