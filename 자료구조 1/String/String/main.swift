d2b8e78ee828bdfe7bf38ebd43e706685598880e
dddowon <dowon4095@naver.com>
2022-11-10 21:58:22 +0900

    문자열 메서드 테스트 2

--- 자료구조 1/String/String/main.swift
+++ 자료구조 1/String/String/main.swift
@@ -51,3 +51,72 @@ print(str[str.index(str.endIndex, offsetBy: -2)]) // D
 //print(str[str.firstIndex(of: str.startIndex)!])
 
 //문자열 메서드 테스트 2
+extension String {
+    func getChar(at index: Int) -> Character {
+        return self[self.index(self.startIndex, offsetBy: index)]
+    }
+}
+
+let str1 = "aBcDe"
+let char1 = str1.getChar(at: 3)
+print(char1) // D
+
+extension String {
+    subscript(_ index: Int) -> Character {
+        return self[self.index(self.startIndex, offsetBy: index)]
+    }
+}
+
+let str2 = "abcde"
+print(str2[1]) // b
+
+let name = "dodo man"
+let firstSpace = name.firstIndex(of: " ") ?? name.endIndex
+let fristName = name[..<firstSpace]
+print(firstSpace) // dodo
+
+extension String {
+    subscript(_ range: Range<Int>) -> String {
+        let fromIndex = self.index(self.startIndex, offsetBy: range.startIndex)
+        let toIndex = self.index(self.startIndex,offsetBy: range.endIndex)
+        return String(self[fromIndex..<toIndex])
+    }
+}
+
+let str3 = "Hello dodo"
+if let range = str3.firstIndex(of: "d") {
+    print(range)
+}
+
+let str4 = "Apple"
+print(str4.contains("a"))
+print(str4.contains("Apple"))
+
+let str5 = "DoDo"
+let str5Arr1 = str5.map{$0}
+print(str5Arr1)
+
+let str5Arr2 = str5.map{String($0)}
+print(str5Arr2)
+
+let array1 = ["d","o","d","o"]
+let arrayStr1 = array1.joined(separator: "")
+print(arrayStr1)
+let arrayStr2 = array1.reduce("", +)
+print(arrayStr2)
+
+let str6 = "aBcDeF"
+//접두어 (앞에서부터 몇 글자)
+print(str6.prefix(3))
+//접미어 (뒤에서부터 몇 글자)
+print(str6.suffix(3))
+
+print(str6.hasPrefix("aB"))
+print(str6.hasPrefix("abc"))
+
+print(str6.hasSuffix("FeD"))
+print(str6.hasSuffix("DeF"))
+
+let str7 = "Hello Swift"
+print(str7.uppercased())
+print(str7.lowercased())
